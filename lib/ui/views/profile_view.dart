1be24f35cf666f305bf3cc6cb8389f5955a75501
Anand kumar <77218580+anand817@users.noreply.github.com>
2021-09-26 16:16:55 +0530

    feat: integrate profile with firebase (#4)

--- lib/ui/views/profile_view.dart
+++ lib/ui/views/profile_view.dart
@@ -1,3 +1,4 @@
+import 'package:borderhacks/enums/view_state.dart';
 import 'package:borderhacks/viewmodels/profile_viewmodel.dart';
 import 'package:borderhacks/ui/views/base_view.dart';
 import 'package:flutter/cupertino.dart';
@@ -17,7 +18,7 @@ class ProfileView extends StatelessWidget {
     );
   }
 
-  Widget _buildProfileImage() {
+  Widget _buildProfileImage(ProfileViewmodel _model) {
     return Positioned(
       bottom: -106,
       child: Column(
@@ -31,11 +32,11 @@ class ProfileView extends StatelessWidget {
             ),
           ),
           Text(
-            'John Doe',
+            _model.name,
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
           Text(
-            'Nuerologist & Gyanacologist',
+            _model.specialisation,
             style: TextStyle(fontSize: 18),
           ),
         ],
@@ -43,13 +44,13 @@ class ProfileView extends StatelessWidget {
     );
   }
 
-  Widget _buildHeader() {
+  Widget _buildHeader(ProfileViewmodel _model) {
     return Stack(
       alignment: Alignment.center,
       clipBehavior: Clip.none,
       children: [
         _buildCoverImage(),
-        _buildProfileImage(),
+        _buildProfileImage(_model),
       ],
     );
   }
@@ -64,12 +65,30 @@ class ProfileView extends StatelessWidget {
     );
   }
 
-  Widget _buildProfileInfo() {
+  Widget _buildProfileInfo(ProfileViewmodel _model) {
     return ListView(
       children: [
-        _buildInfo(Icons.timer, 'Timing', '8:00AM - 5:00PM'),
-        _buildInfo(Icons.money, 'Appointment fees', 'Rs 1000000000000/-'),
-        _buildInfo(Icons.location_city, 'Address', 'Kahi nhi'),
+        _buildInfo(Icons.timer, 'Timing', _model.timing),
+        _buildInfo(Icons.money, 'Appointment fees', 'Rs ${_model.fees}/-'),
+        _buildInfo(Icons.location_city, 'Address', _model.address),
+      ],
+    );
+  }
+
+  Widget _buildProfile(ProfileViewmodel _model) {
+    return Column(
+      children: [
+        Expanded(
+          flex: 1,
+          child: _buildHeader(_model),
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            margin: EdgeInsets.only(top: 120),
+            child: _buildProfileInfo(_model),
+          ),
+        )
       ],
     );
   }
@@ -77,21 +96,15 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BaseView<ProfileViewmodel>(
-      builder: (context, model, child) => Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: _buildHeader(),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              margin: EdgeInsets.only(top: 120),
-              child: _buildProfileInfo(),
-            ),
-          )
-        ],
-      ),
+      onModelReady: (model) {
+        model.fetchProfile();
+      },
+      builder: (context, model, child) {
+        if (model.state == ViewState.Busy) {
+          return Center(child: CircularProgressIndicator());
+        }
+        return _buildProfile(model);
+      },
     );
   }
 }
